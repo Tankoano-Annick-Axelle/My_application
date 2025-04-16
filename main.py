78a842a2709a74a5cfcb2bfa6f3a04d6f0588651
Tankoano-Annick-Axelle <tankoano.yentemaannickaxelle8851.fsts@uhp.ac.ma>
2025-04-16 17:27:30 +0100

    variables d'environnement

--- main.py
+++ main.py
@@ -25,10 +25,10 @@ app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=int(os.getenv('SESS
 app.config['SESSION_PERMANENT'] = True
 
 # Configuration de l'email
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
+app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT'))
+app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS')
+app.config['MAIL_USE_SSL'] = os.getenv('MAIL_USE_SSL')
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
 app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_DEFAULT_SENDER')
